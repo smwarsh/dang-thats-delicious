4368eec6a5fdc7d34914d5e46f010b248c0de936
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-11-25 19:12:46 -0500

    Learning about routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  const steph = { name: 'Steph', age: 2, cool: true };
+  res.json(steph);
 });
 
 module.exports = router;
