2acfd9e205559c34b73357655b3cf2ef187e3328
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-11-27 18:46:49 -0500

    Learning about pug, and rendering in Express

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
 router.get('/', (req, res) => {
   // res.send('Hey! It works!');
   const steph = { name: 'Steph', age: 2, cool: true };
-  res.json(steph);
+  // res.json(steph);
+  res.render('hello', {
+    name: 'steph',
+    cat: req.query.cat
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
