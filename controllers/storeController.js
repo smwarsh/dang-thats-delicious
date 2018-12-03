be49f74d6636de5b49c7342fc7ddcc4c2bb46130
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-12-02 22:34:11 -0500

    Clean up & start with Store model

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,11 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Steph';
-  if(req.name === 'Steph') {
-    throw Error('No. There is only one Steph.');
-  }
-  next();
-}
-
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
