f6756b9ca9b8ae8a0afd0481b7a9d59e3ca982c1
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-12-16 20:37:04 -0500

    Start connecting the database

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
@@ -8,5 +11,6 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = (req, res) => {
-  res.json(req.body);
+  const store = new Store(req.body);
+  store.save();
 }
