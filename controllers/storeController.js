9fc24dc58d61f7096c7c32fad74f13913c49fa61
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-12-17 22:22:17 -0500

    Error handling & async await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,7 +10,8 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 }
 
-exports.createStore = (req, res) => {
+exports.createStore = async (req, res) => {
   const store = new Store(req.body);
-  store.save();
+  await store.save();
+  res.redirect('/');
 }
