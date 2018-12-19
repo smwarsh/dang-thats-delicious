48d93fd4faf857001a0319c4d2475f6c5cd58407
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-12-19 13:40:58 -0500

    Playing around with flashes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -3,6 +3,10 @@ const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
   console.log(req.name);
+  req.flash('error', 'Something happened');
+  req.flash('info', 'Something happened');
+  req.flash('warning', 'Something happened');
+  req.flash('success', 'Something happened');
   res.render('index');
 }
 
@@ -13,5 +17,6 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
   const store = new Store(req.body);
   await store.save();
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect('/');
 }
