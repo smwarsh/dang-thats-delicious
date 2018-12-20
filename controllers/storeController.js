3c177b0c3cf732e521b68350e4b2a397455fe495
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-12-19 21:29:55 -0500

    Clean up async await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
-  req.flash('error', 'Something happened');
-  req.flash('info', 'Something happened');
-  req.flash('warning', 'Something happened');
-  req.flash('success', 'Something happened');
   res.render('index');
 }
 
@@ -15,8 +10,7 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
+  const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
-  res.redirect('/');
+  res.redirect(`/store/${store.slug}`);
 }
