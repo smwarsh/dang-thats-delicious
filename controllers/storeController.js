3309dc1b17155ec5c1498a5bb94dd302be4ea2ad
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-12-23 11:11:10 -0500

    Add storeCard to page
    Yay! I did it!

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -18,6 +18,5 @@ exports.createStore = async (req, res) => {
 exports.getStores = async (req, res) => {
   // 1. Query the database for a list of all stores
   const stores = await Store.find();
-  console.log(stores);
   res.render('stores', { title: 'Stores', stores });
 }
