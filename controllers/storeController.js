604abbc966ec1dc490f306f04dec206b783d8c6b
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2019-04-08 21:04:24 -0400

    Set up multer and options

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That filetype isn\'t allowed!' }, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
   res.render('index');
