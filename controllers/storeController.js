6ecbf7502889ba6464cd72a479ba3a86edc6956d
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2019-04-08 21:11:56 -0400

    Set form to accept picture

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -22,6 +22,8 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 }
 
+exports.upload = multer(multerOptions).single('photo');
+
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
